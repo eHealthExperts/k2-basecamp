f48dad16f73f13d3d012480032228a79576efc62
Christian Fochler <fochler@ehealhtexperts.de>
2017-04-28 16:06:27 +0200

    logger refactored - handle line ending for logger configuration - smaller functions

--- src/logging.rs
+++ src/logging.rs
@@ -12,41 +12,51 @@ use std::sync::{Once, ONCE_INIT};
 static INIT: Once = ONCE_INIT;
 
 pub fn init() {
-    INIT.call_once(|| match var("K2_LOG_PATH") {
-                       Ok(path) => {
-        let file = FileAppender::builder()
-            .encoder(Box::new(PatternEncoder::new("{d} {l} {M}: {m}{n}")))
-            .build(path + &"/ctehxk2.log".to_string())
-            .unwrap();
+    INIT.call_once(|| init_logger());
+}
 
-        let config = Config::builder()
-            .appender(Appender::builder().build("file", Box::new(file)))
-            .logger(Logger::builder()
-                        .appender("file")
-                        .additive(false)
-                        .build("ctehxk2", LogLevelFilter::Debug))
-            .build(Root::builder()
-                       .appender("file")
-                       .build(LogLevelFilter::Error))
-            .unwrap();
+fn init_logger() {
+    let config = match var("K2_LOG_PATH") {
+        Ok(path) => init_file_logger(String::from(path)),
+        _ => init_stdout_logger(),
+    };
 
-        log4rs::init_config(config).unwrap();
+    log4rs::init_config(config).unwrap();
+}
+
+fn init_file_logger(mut path: String) -> Config {
+    if !path.trim().ends_with("/") {
+        path.push_str("/");
     }
-                       _ => {
-        let stdout = ConsoleAppender::builder().build();
 
-        let config = Config::builder()
-            .appender(Appender::builder().build("stdout", Box::new(stdout)))
-            .logger(Logger::builder()
-                        .appender("stdout")
-                        .additive(false)
-                        .build("ctehxk2", LogLevelFilter::Debug))
-            .build(Root::builder()
-                       .appender("stdout")
-                       .build(LogLevelFilter::Error))
-            .unwrap();
+    let file = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} {l} {M}: {m}{n}")))
+        .build(path + &"ctehxk2.log".to_string())
+        .unwrap();
 
-        log4rs::init_config(config).unwrap();
-    }
-                   })
+    Config::builder()
+        .appender(Appender::builder().build("file", Box::new(file)))
+        .logger(Logger::builder()
+                    .appender("file")
+                    .additive(false)
+                    .build("ctehxk2", LogLevelFilter::Debug))
+        .build(Root::builder()
+                   .appender("file")
+                   .build(LogLevelFilter::Error))
+        .unwrap()
+}
+
+fn init_stdout_logger() -> Config {
+    let stdout = ConsoleAppender::builder().build();
+
+    Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .logger(Logger::builder()
+                    .appender("stdout")
+                    .additive(false)
+                    .build("ctehxk2", LogLevelFilter::Debug))
+        .build(Root::builder()
+                   .appender("stdout")
+                   .build(LogLevelFilter::Error))
+        .unwrap()
 }
