64eaf201de2c4356ddd580d529e462cbab22162f
Christian Fochler <fochler@ehealhtexperts.de>
2017-04-28 17:18:40 +0200

    extract request path building and ok response handling into seperate functions

--- src/ctapi/close.rs
+++ src/ctapi/close.rs
@@ -3,6 +3,7 @@ extern crate hyper;
 pub use self::super::{ERR_HOST, ERR_HTSI, ERR_INVALID, MAP, OK};
 pub use self::super::super::{http, logging};
 
+use hyper::client::response::Response;
 use hyper::status::StatusCode;
 use std::io::Read;
 
@@ -18,16 +19,9 @@ pub fn close(ctn: u16) -> i8 {
         return ERR_INVALID;
     }
 
-    // Build the request URL
-    let pn = MAP.lock().unwrap().get(&ctn).unwrap().clone();
-    let mut path = String::from("ct_close");
-    path.push_str("/");
-    path.push_str(&ctn.to_string());
-    path.push_str("/");
-    path.push_str(&pn.to_string());
-
+    let path = get_request_path(ctn);
     // Perform the request
-    let mut response = match http::simple_post(path) {
+    let response = match http::simple_post(path) {
         Ok(response) => response,
         Err(error) => {
             debug!("Error: {:?}", error);
@@ -39,24 +33,37 @@ pub fn close(ctn: u16) -> i8 {
     debug!("{:?}", response); // TODO enrich output
 
     match response.status {
-        StatusCode::Ok => {
-            // Cast server response
-            let mut body = String::new();
-            response.read_to_string(&mut body).unwrap();
-
-            let status = body.parse::<i8>().unwrap();
-            if status == OK {
-                // Remove CTN
-                MAP.lock().unwrap().remove(&ctn);
-                debug!("CT_close: Card terminal has been closed.");
-            }
-
-            debug!("CT_close: Returning {}", status);
-            status
-        }
+        StatusCode::Ok => handle_ok_status(response, ctn),
         _ => {
             error!("CT_close: Response not OK! Returning {}", ERR_HOST);
             ERR_HOST
         }
     }
+}
+
+
+fn get_request_path(ctn: u16) -> String {
+    let pn = MAP.lock().unwrap().get(&ctn).unwrap().clone();
+    let mut path = String::from("ct_close");
+    path.push_str("/");
+    path.push_str(&ctn.to_string());
+    path.push_str("/");
+    path.push_str(&pn.to_string());
+
+    path
+}
+
+fn handle_ok_status(mut response: Response, ctn: u16) -> i8 {
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+
+    let status = body.parse::<i8>().unwrap();
+    if status == OK {
+        // Remove CTN
+        MAP.lock().unwrap().remove(&ctn);
+        debug!("CT_close: Card terminal has been closed.");
+    }
+
+    debug!("CT_close: Returning {}", status);
+    status
 }
