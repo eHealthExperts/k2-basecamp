0d288420c88a650bf082b71620a05ed6c3d1c13a
Christian Fochler <fochler@ehealhtexperts.de>
2017-05-04 09:50:27 +0200

    add rust written unit tests for the http mod

--- src/http.rs
+++ src/http.rs
@@ -1,3 +1,5 @@
+#[cfg(test)]
+extern crate http_stub;
 extern crate hyper;
 extern crate log;
 extern crate serde;
@@ -11,6 +13,7 @@ use hyper::mime::{Mime, TopLevel, SubLevel};
 use serde::Serialize;
 use std::env::var;
 
+const BASE_URL_KEY: &'static str = "K2_BASE_URL";
 const BASE_URL: &'static str = "http://localhost:8080/k2/ctapi/";
 
 #[derive(Serialize)]
@@ -40,7 +43,7 @@ pub fn post<T>(path: String, payload: &T) -> Result<Response, Error>
 }
 
 fn get_request_url(path: String) -> String {
-    let mut url = var("K2_BASE_URL").unwrap_or(BASE_URL.to_string());
+    let mut url = var(BASE_URL_KEY).unwrap_or(BASE_URL.to_string());
     if !url.trim().ends_with("/") {
         url.push_str("/");
     }
@@ -48,4 +51,102 @@ fn get_request_url(path: String) -> String {
     url.push_str(&path);
 
     url
+}
+
+#[cfg(test)]
+mod tests {
+    use self::http_stub as hs;
+    use self::http_stub::HttpStub;
+    use super::*;
+    use std::env;
+    use std::io::Read;
+
+    #[test]
+    fn simple_post_is_returning_error() {
+        env::remove_var(BASE_URL_KEY);
+
+        let res = simple_post(String::from("hello"));
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn simple_post_is_sending_empty_body() {
+        let url = HttpStub::run(|mut stub| {
+                                    stub.got_body("");
+                                    stub.got_path("/hello");
+                                    stub.got_method(hs::Method::Post);
+
+                                    stub.send_status(hs::StatusCode::Ok);
+                                    stub.send_body("world");
+                                });
+
+        env::set_var(BASE_URL_KEY, url);
+
+        let mut res = simple_post(String::from("hello")).unwrap();
+        assert_eq!(res.status, hyper::Ok);
+
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        assert_eq!(body, "world");
+    }
+
+    #[test]
+    fn post_is_returning_error() {
+        env::remove_var(BASE_URL_KEY);
+
+        #[derive(Serialize)]
+        struct Data {
+            name: String,
+        };
+
+        let data = Data { name: String::from("hello") };
+
+        let res = post(String::from("hello"), &data);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn post_is_sending_payload_as_json_in_body() {
+        let url = HttpStub::run(|mut stub| {
+            stub.got_path("/hello");
+            stub.got_method(hs::Method::Post);
+            stub.got_header("content-type", "application/json");
+            stub.got_body(r#"\{"name":"hello"\}"#);
+
+            stub.send_status(hs::StatusCode::Ok);
+            stub.send_body("world");
+        });
+
+        env::set_var(BASE_URL_KEY, url);
+
+        #[derive(Serialize)]
+        struct Data {
+            name: String,
+        };
+
+        let data = Data { name: String::from("hello") };
+
+        let mut res = post(String::from("hello"), &data).unwrap();
+        assert_eq!(res.status, hyper::Ok);
+
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        assert_eq!(body, "world");
+    }
+
+    #[test]
+    fn get_request_url_concats_default_value_of_base_url_key_and_given_string() {
+        env::remove_var(BASE_URL_KEY);
+
+        let url = get_request_url(String::from("a"));
+        assert_eq!(url, BASE_URL.to_owned() + "a");
+    }
+
+    #[test]
+    fn get_request_url_concats_value_of_base_url_key_and_given_string() {
+        env::set_var(BASE_URL_KEY, "abc");
+
+        let url = get_request_url(String::from("a"));
+        assert_eq!(url, "abc/a");
+    }
 }
