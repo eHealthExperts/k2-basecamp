5124fe9f9d4c8cd691aeef09bc6c869acd01118a
Christian Fochler <fochler@ehealhtexperts.de>
2017-05-09 13:19:07 +0200

    extend config mod by http k2_base_url handling

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,9 @@ extern crate envy;
 
 use std::env::var;
 
+const BASE_URL_KEY: &'static str = "K2_BASE_URL";
+const BASE_URL: &'static str = "http://localhost:8080/k2/ctapi/";
+
 #[derive(Deserialize)]
 struct CtnPn {
     #[serde(rename="K2_CTN")]
@@ -9,6 +12,15 @@ struct CtnPn {
 
     #[serde(rename="K2_PN")]
     pn: u16,
+}
+
+pub fn base_url() -> String {
+    let mut url = var(BASE_URL_KEY).unwrap_or(BASE_URL.to_string());
+    if !url.trim().ends_with("/") {
+        url.push_str("/");
+    }
+
+    url
 }
 
 pub fn ctn_or(ctn: u16) -> u16 {
@@ -80,5 +92,26 @@ mod tests {
         let pn = pn_or(1);
 
         assert_eq!(pn, 1);
+    }
+
+    #[test]
+    fn base_url_return_env_value() {
+        env::set_var("K2_BASE_URL", "a/");
+        let url = base_url();
+
+        assert_eq!(url, "a/");
+
+        env::set_var("K2_BASE_URL", "1");
+        let url = base_url();
+
+        assert_eq!(url, "1/");
+    }
+
+    #[test]
+    fn base_url_return_default_value_if_no_env() {
+        env::remove_var("K2_BASE_URL");
+        let url = base_url();
+
+        assert_eq!(url, BASE_URL);
     }
 }
