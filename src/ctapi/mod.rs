6283c7b3266e5235d95d178ccdee93517ab8e70f
Christian Fochler <mail@christianfochler.de>
2017-06-30 14:32:02 +0200

    adjust statment order

--- src/ctapi/mod.rs
+++ src/ctapi/mod.rs
@@ -6,11 +6,11 @@ pub use self::status::StatusCode;
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-lazy_static! {
-    pub static ref MAP: Mutex<HashMap<u16, u16>> = Mutex::new(HashMap::new());
-}
-
 mod close;
 mod data;
 mod init;
 mod status;
+
+lazy_static! {
+    pub static ref MAP: Mutex<HashMap<u16, u16>> = Mutex::new(HashMap::new());
+}
