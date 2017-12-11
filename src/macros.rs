a18f152fdf147effb4d1c02b05dfc724b6c9887d
Christian Fochler <mail@christianfochler.de>
2017-12-11 15:05:41 +0100

    revise macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,32 +1,38 @@
 macro_rules! test_server {
-    ({($method:ident) ($($url:tt)+) => $handle:block}) => ({
-        let addr = String::from("127.0.0.1:54321");
-        ::std::env::set_var("K2_BASE_URL", format!("{}{}", "http://", &addr));
+    (
+        $(
+            ($request:ident : &Request)
+            $body: block
+        )+
+    ) => {
+        $(
+            {
+                #[allow(unused)]
+                let server = ::rouille::Server::new("127.0.0.1:0", move | $request | {
+                    $body
+                }).unwrap();
 
-        let server = ::rouille::Server::new(addr.clone(), move |request| {
-            router!(request,
-                ($method) ($($url)+) => $handle,
-                _ => {
-                    assert!(false);
-                    ::rouille::Response::empty_404()
-                }
-            )
-        }).unwrap();
+                ::std::env::set_var(
+                    "K2_BASE_URL",
+                    format!("{}{}", "http://", &server.server_addr()
+                ));
 
-        let (tx, rx) = ::std::sync::mpsc::channel();
+                let (tx, rx) = ::std::sync::mpsc::channel();
 
-        ::std::thread::spawn(move || loop {
-            server.poll();
-            match rx.try_recv() {
-                Ok(_) | Err(::std::sync::mpsc::TryRecvError::Disconnected) => {
-                    break;
-                }
-                Err(::std::sync::mpsc::TryRecvError::Empty) => {}
+                ::std::thread::spawn(move || loop {
+                    server.poll();
+                    match rx.try_recv() {
+                        Ok(_) | Err(::std::sync::mpsc::TryRecvError::Disconnected) => {
+                            break;
+                        }
+                        Err(::std::sync::mpsc::TryRecvError::Empty) => {}
+                    }
+                });
+
+                tx
             }
-        });
-
-        tx
-    })
+        )+
+    }
 }
 
 macro_rules! hashmap {
